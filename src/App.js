d7985747901def63091b6ecf98237e8325955120
Bashar Daseh <bdaseh@unisynvoting.com>
2024-11-06 14:17:30 -0800

    Interview cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import UserProfile from './UserProfile';
+import Timer from './Timer';
 
 function App() {
   return (
     <div className="App">
       <UserProfile />
+      {/* <Timer /> */}
     </div>
   );
 }
--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -4,14 +4,16 @@ function UserProfile() {
   const [state, setState] = useState();
 
   useEffect(() => {
+
     // Simulate a 2 Second delay in setting user data
  
       setState({
-        user: {
-          name: "Alice"
-        }
+            user: {
+              name: "Alice",
+              score: 0
+            } 
       });
- 
+      
   }, []);
 
   // Second step is to display the user's name or "Loading..."
@@ -20,6 +22,7 @@ function UserProfile() {
       <h2>User Profile</h2>
       {/* Safely display the user's name or "Loading..." */}
       <p>Name: { /* Your code here */ }</p>
+      <p>Score: { /* Your code here */ }</p>
     </div>
   );
 }
